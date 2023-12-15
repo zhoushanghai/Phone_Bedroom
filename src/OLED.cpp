16ffbf8a6ca5f268741a7a2677154c2ae59803f8
hz <1281989921@qq.com>
2023-12-15 20:45:02 +0800

    V1.2 oledshow data

--- src/OLED.cpp
+++ src/OLED.cpp
@@ -9,7 +9,7 @@ U8G2_SSD1306_128X64_NONAME_F_HW_I2C u8g2(U8G2_R0, /*reset=*/U8X8_PIN_NONE, /*clo
 int progess = 0;
 
 #define MENU_SIZE 3
-char *menu[MENU_SIZE] = {"SHT40 Temp:", "SHT40 Humi:", "MAX667Temp:"};
+char const *menu[MENU_SIZE] = {"SHT40 Temp:", "SHT40 Humi:", "MAX667Temp:"};
 
 void OLED096_setup()
 {
